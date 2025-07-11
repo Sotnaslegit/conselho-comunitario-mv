15a32ef3885dcf2107f746af7fa95e9c38fbf637
IGOR SILVA SCHIMITZ <igor_s_schimitz@estudante.sesisenai.org.br>
2025-07-10 21:27:22 -0300

    feat:teste se troca pagina

--- cypress/e2e/ccmv.cy.js
+++ cypress/e2e/ccmv.cy.js
@@ -1,5 +1,19 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://sotnaslegit.github.io/conselho-comunitario-mv/index.html')
+  })
+
+  it('se troca as paginas', () => {
+    cy.visit('https://sotnaslegit.github.io/conselho-comunitario-mv/index.html')
+   
+    cy.get(':nth-child(2) > .nav-link').click()
+    cy.contains('Em 15/04/1981').should('exist')
+
+    cy.get(':nth-child(3) > .nav-link').click()
+    cy.contains('Jiu Jitsu').should('exist')
+
+    cy.get(':nth-child(4) > .nav-link').click()
+    cy.contains('WhatsApp').should('exist')
+
   })
 })
