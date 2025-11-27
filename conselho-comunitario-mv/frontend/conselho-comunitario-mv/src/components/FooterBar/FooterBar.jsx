47b64ce9fe99907b988691efd778ddcc2d5848d1
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-11-26 21:47:04 -0300

    .\pages\

--- conselho-comunitario-mv/frontend/conselho-comunitario-mv/src/components/FooterBar/FooterBar.jsx
+++ conselho-comunitario-mv/frontend/conselho-comunitario-mv/src/components/FooterBar/FooterBar.jsx
@@ -9,41 +9,45 @@ const FooterBar = () => {
     return (
         <>
             <div className="footer">
-                <div className="d-flex justify-content-around">
-
-                    <div className="mb-4 text-center">
-                        <h5>Redes Sociais</h5>
-
-                        <div className="social-icons">
-                            <div className="d-flex">
-                                <a href="https://w.app/xocq7k"><img src={whatsapp} alt="" /></a>
-                                <a href="https://www.facebook.com/groups/moradoreseamigosdobairromv"><img src={facebook} alt="" /></a>
-                                <a href="https://www.instagram.com/conselhoccmv/"><img src={instagram} alt="" /></a>
+                <div className="container-fluid d-flex justify-content-around">
+                    <div className="container mb-6 d-flex justify-content-around flex-column">
+                        <div style={{display: "flex", gap: "50px", "flexDirection": "column", "margin": "0 auto"}}>
+                            <div className="container mb-6 -flex">
+                                <h5>Redes Sociais</h5>
+                                <div className="social-icons">
+                                    <div className="d-flex">
+                                        <a href="https://w.app/xocq7k"><img src={whatsapp} alt="" /></a>
+                                        <a href="https://www.facebook.com/groups/moradoreseamigosdobairromv"><img src={facebook} alt="" /></a>
+                                        <a href="https://www.instagram.com/conselhoccmv/"><img src={instagram} alt="" /></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="container mb-6">
+                                <h5>Fale Conosco:</h5>
+                                <div className="ctt">
+                                    <label><img src={call} alt="" /></label>
+                                    <p>48 98501-5251</p>
+                                </div>
+                                <div className="ctt">
+                                    <label><img src={email} alt="" /> </label>
+                                    <p>ccmv.adm@gmail.com</p>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="mb-4 text-center">
+
+
+                    <div className="container mb-6 text-center mt-5">
                         <h5>Onde estamos</h5>
                         <p>R. do Guaramirim, 200<br />Monte Verde, Florianópolis - SC, 88032-480
                         </p>
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3537.15589499155!2d-48.497485894009635!3d-27.55767184350902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x952738a3a215baef%3A0x9ad1986d8bc6359!2sConselho%20Comunit%C3%A1rio%20Monte%20Verde!5e0!3m2!1spt-BR!2sbr!4v1750897792662!5m2!1spt-BR!2sbr"
-                            width="300" height="225" allowFullScreen="" loading="lazy"
+                            width="300" height="200" allowFullScreen="" loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
 
-                    <div className="mb-4 text-center">
-                        <h5>Fale Conosco:</h5>
-                        <div className="ctt">
-                            <label><img src={call} alt="" /></label>
-                            <p>48 98501-5251</p>
-                        </div>
-                        <div className="ctt">
-                            <label><img src={email} alt="" /> </label>
-                            <p>ccmv.adm@gmail.com</p>
-                        </div>
-                    </div>
                 </div>
             </div>
         </>
--- conselho-comunitario-mv/frontend/conselho-comunitario-mv/src/components/NewsPage/NewsPage.jsx
+++ conselho-comunitario-mv/frontend/conselho-comunitario-mv/src/components/NewsPage/NewsPage.jsx
@@ -3,45 +3,6 @@ const NewsPage = () => {
         <>
             <h1 class="text-center primary-font fw-bold">NOTÍCIAS</h1>
             <div class="container">
-                <div class="card">
-                    <div class="card-container">
-                        <div class="card-image">
-                            <img width="100%" src="../assets/imgs/pesos.JPG" alt="" />
-                        </div>
-                        <div class="card-description">
-                            <h3 class="text-center">Título</h3>
-                            <p>Descrição</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="container">
-                <div class="card">
-                    <div class="card-container">
-                        <div class="card-image">
-                            <img width="100%" src="../assets/imgs/queda-braco.JPG" alt="" />
-                        </div>
-                        <div class="card-description">
-                            <h3 class="text-center">Título</h3>
-                            <p>Descrição</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="container">
-                <div class="card">
-                    <div class="card-container">
-                        <div class="card-image">
-                            <img width="100%" src="../assets/imgs/campo-lateral.JPG" alt="" />
-                        </div>
-                        <div class="card-description">
-                            <h3 class="text-center">Título</h3>
-                            <p>Descrição</p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </>
     )
